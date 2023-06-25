544ac4e154af98b324e74b83d88f212c38b477a7
Ankush <ankush.singh2582@gmail.com>
2023-06-25 17:31:36 +0530

    configur urls, making room model,craeted views

--- spotifyBackened/API/models.py
+++ spotifyBackened/API/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+# generate random string 
+def generate_unique_room():
+    import random
+    import string
+    while True:
+        unique_room= "".join(random.choices(string.ascii_uppercase,k=6))
+        if Room.objects.filter(room_code=unique_room):
+            continue
+        else:
+            break
+    return unique_room
+
+        
+
+
 # Create your models here.
+class Room(models.Model):
+    room_code=models.CharField(max_length=10,default=None, unique=True)
+    host_name=models.CharField(max_length=20,unique=True)
+    guest_can_stop= models.BooleanField(default=False)
+    votes_to_change=models.IntegerField(default=1)
+    created_on=models.DateTimeField(auto_now_add=True)
+
--- spotifyBackened/API/views.py
+++ spotifyBackened/API/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+
+def main(request):
+    print("hello")
+    return HttpResponse("Ankkm")
 
 # Create your views here.
